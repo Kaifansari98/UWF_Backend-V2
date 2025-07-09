dc176817895df2c48d842116493cbf3de74c7088
kaifansari98 <kaifansari54540@gmail.com>
2025-07-10 02:12:47 +0530

    Dashboard API's has been ready and next we haveto implement it

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
 import formRoutes from './form.routes';
 import formSubmissionRoutes from './formSubmission.routes';
+import dashboardRoutes from './dashboard.routes'
 
 const router = Router();
 
@@ -10,6 +11,7 @@ router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/forms', formRoutes);
 router.use('/', formSubmissionRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.get('/health', (_req, res) => {
   res.status(200).json({ status: 'OK', message: 'API is healthy' });
